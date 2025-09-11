e24239d01a6f95635638906d5a267538aa776b8c
togb-reeper <acebany080@gmail.com>
2025-09-11 08:53:07 +0200

    stack navigation setup

--- app/screens/SplashScreen.tsx
+++ app/screens/SplashScreen.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
 import React from 'react'
 import { Colors } from '@/utils/Colors'
 import { StatusBar } from 'expo-status-bar'
