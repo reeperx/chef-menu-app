afd3622aae4222edeba46ec38426d78b6f9a7d38
togb-reeper <acebany080@gmail.com>
2025-09-12 04:36:36 +0200

    icons aded to bottom nav

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,12 +5,16 @@ export default function TabsLayout() {
   return (
     <Tabs
       screenOptions={({ route }) => ({
-        tabBarIcon: ({ color, size }) => {
+        tabBarIcon: ({ color, size, focused }) => {
           let iconName = "home-outline";
-          if (route.name === "index") iconName = "home-outline";
-          else if (route.name === "cart") iconName = "cart-outline";
-          else if (route.name === "favorite") iconName = "heart-outline";
-          else if (route.name === "profile") iconName = "person-outline";
+          if (route.name === "index")
+            iconName = focused ? "home" : "home-outline";
+          else if (route.name === "cart")
+            iconName = focused ? "cart" : "cart-outline";
+          else if (route.name === "favorite")
+            iconName = focused ? "heart" : "heart-outline";
+          else if (route.name === "profile")
+            iconName = focused ? "person" : "person-outline";
           return <Ionicons name={iconName as any} size={size} color={color} />;
         },
         tabBarActiveTintColor: "#f16e03ff",
@@ -24,12 +28,12 @@ export default function TabsLayout() {
           title: "Home",
         }}
       />
-        <Tabs.Screen
-          name="favorite"
-          options={{
-            title: "Favorite",
-          }}
-        />
+      <Tabs.Screen
+        name="favorite"
+        options={{
+          title: "Favorite",
+        }}
+      />
       <Tabs.Screen
         name="cart"
         options={{
